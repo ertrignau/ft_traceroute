c7e0c523fe1f9db6faa959af542ea6f3a0640310
ertrigna <ertrigna@f6r8s8.paris.42.school>
2026-06-05 12:04:15 +0200

    visu branch

--- src/traceroute.c
+++ src/traceroute.c
@@ -6,7 +6,7 @@
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 16:09:51 by eric              #+#    #+#             */
-/*   Updated: 2026/06/05 11:55:47 by ertrigna         ###   ########.fr       */
+/*   Updated: 2026/06/05 12:03:38 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,37 +68,59 @@ void	run_traceroute(t_traceroute *trace)
 	int					ret;
 	int					done;
 	int					first_printed;
+	double				rtt1, rtt2, rtt3;
+	int					timeout_count;
 
 	done = 0;
 	printf("traceroute to %s, %d hops max\n", inet_ntoa(trace->dest.sin_addr), trace->max_ttl);
 	while (trace->ttl <= trace->max_ttl && !done)
 	{
-		printf(" %2d  ", trace->ttl);
-		probe = 0;
-		first_printed = 0;
-		ft_memset(&first_addr, 0, sizeof(first_addr));
-		while (probe < trace->probes_per_ttl)
-		{
-			send_probe(trace);
-			ret = wait_response(trace, &recv_addr, buf);
-			if (ret == -1)
-				print_timeout();
-			else
-			{
-				if (!first_printed || recv_addr.sin_addr.s_addr != first_addr.sin_addr.s_addr)
-					print_hop(&recv_addr, trace);
-				else
-					printf("%.3f ms  ", calc_rtt(trace));
-				first_addr = recv_addr;
-				first_printed = 1;
-				if (ret == ICMP_UNREACH)
-					done = 1;
-			}
-			probe++;
-		}
-		if (trace->json_output == 1)
-			output_hop_data(trace, trace->ttl, first_addr.sin_addr.s_addr?, ...);
-		printf("\n");
-		trace->ttl++;
-	}
+    	printf(" %2d  ", trace->ttl);
+    	probe = 0;
+    	first_printed = 0;
+    	rtt1 = 0; rtt2 = 0; rtt3 = 0;  // Reset pour ce TTL
+   		timeout_count = 0;
+    	ft_memset(&first_addr, 0, sizeof(first_addr));
+    
+    	while (probe < trace->probes_per_ttl)
+    	{
+        	send_probe(trace);
+        	ret = wait_response(trace, &recv_addr, buf);
+        	if (ret == -1)
+        	{
+            	timeout_count++;
+            	print_timeout();
+        	}
+        	else
+        	{
+            	double rtt = calc_rtt(trace);
+            	// Stocker les RTT pour JSON output
+            	if (probe == 0) 
+					rtt1 = rtt;
+            	else if (probe == 1) 
+					rtt2 = rtt;
+            	else if (probe == 2) 
+					rtt3 = rtt;
+            	if (!first_printed || recv_addr.sin_addr.s_addr != first_addr.sin_addr.s_addr)
+                	print_hop(&recv_addr, trace);
+            	else
+                	printf("%.3f ms  ", rtt);
+            	first_addr = recv_addr;
+            	first_printed = 1;
+            	if (ret == ICMP_UNREACH)
+                	done = 1;
+        	}
+        	probe++;
+    	}
+    // Output JSON si flag activé
+    if (trace->json_output && first_printed)
+    {
+        char ip_str[INET_ADDRSTRLEN];
+        char hostname[256];
+        strcpy(ip_str, inet_ntoa(first_addr.sin_addr));
+        getnameinfo((struct sockaddr *)&first_addr, sizeof(first_addr), hostname, sizeof(hostname), NULL, 0, 0);
+        output_hop_data(trace, trace->ttl, ip_str, hostname, rtt1, rtt2, rtt3, (timeout_count == 3));
+    }
+    printf("\n");
+    trace->ttl++;
 }
